80a9552755f0b7ef0b401102e9b44c206b83c602
Ethan Cheng <ethan@nijika.org>
2026-09-11 01:57:40 -0700

    add test script

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+TEST_LINKS=test-links.txt
+
+paste -d '' <(yes "http://localhost:3000/lowes?productUrl=" | head -n $(wc -l "$TEST_LINKS" | grep -oE '[0-9]*')) "$TEST_LINKS" |\
+	xargs -L 4 -- curl
